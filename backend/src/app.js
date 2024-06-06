864deff7cba8769257a90a37dcda8cc0fe8bb31f
Vitor Sales <vitormalisa@gmail.com>
2024-06-06 16:41:47 -0300

    Feat: Correction in connection database.

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,4 +8,8 @@ app.get('/', (_request, response) => {
   response.json({ status: 'Store Manager UP!' });
 });
 
+app.get('/products', (req, res) => {
+
+});
+
 module.exports = app;
--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createPool({
   port: process.env.MYSQL_PORT || 33060,
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
-  database: 'trybecardb',
+  database: 'StoreManager',
 });
 
 module.exports = connection;
