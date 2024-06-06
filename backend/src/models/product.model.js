8ecbc2c5d983e76b69f4a898efe3edf9bf121095
Vitor Sales <vitormalisa@gmail.com>
2024-06-06 16:49:27 -0300

    Feat: product.model.js -> findAll findById

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,18 @@
+const connection = require('./connection');
+
+const findAll = async () => {
+  const query = 'SELECT * FROM products';
+  const [products] = await connection.execute(query);
+  return products;
+};
+
+const findById = async (productId) => {
+  const query = 'SELECT * FROM products WHERE id = ?';
+  const [[product]] = await connection.execute(query, [productId]);
+  return product;
+};
+
+module.exports = {
+  findAll,
+  findById,
+};
